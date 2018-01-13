83155ffc98f2b893dc53d295a99979db6ded9075
vcordero07 <vcordero07@gmail.com>
2018-01-13 10:23:13 -0500

    wip

---
+++ src/components/surprise-button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SurpriseButton(props) {
+    return <button>Surprise!</button>;
+}
---
+++ src/components/surprise-image.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SurpriseImage() {
+    return <img src="https://media.giphy.com/media/10dV5LFkPenFxm/giphy.gif" alt="A surprising fig" />;
+}
